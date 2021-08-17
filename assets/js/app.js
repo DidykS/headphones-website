4ba86edbb0bc8e56a2a3ba486ea22731511f8ed8
Yaroslav Didyk <slavan.didyk@gmail.com>
2021-08-17 22:05:35 +0300

    create a fixed header

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,9 @@
-/* show menu funtion */
+const header = document.querySelector("#header")
 const nav = document.querySelector("#nav")
 const closeMenu = document.querySelector("#nav__close")
 const showMenu = document.querySelector("#show__menu")
 
+/* show menu funtion */
 // show menu 
 showMenu.addEventListener("click", function() {
   nav.classList.add("active")
@@ -11,4 +12,17 @@ showMenu.addEventListener("click", function() {
 // close menu
 closeMenu.addEventListener("click", function() {
   nav.classList.remove("active")
+})
+
+
+// fixed header
+window.addEventListener("scroll", function() {
+  let scrollPos = window.scrollY
+
+  if (scrollPos > 500) {
+    header.classList.add("fixed")
+  }
+  else {
+    header.classList.remove("fixed")
+  }
 })
